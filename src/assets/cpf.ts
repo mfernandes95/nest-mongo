5fa765a57972d217c301b4e03ae5f9281c333667
Livia Dantas <livia.ss.dantas@gmail.com>
2020-12-01 10:56:03 -0300

    cpf validate function refactored

--- src/assets/cpf.ts
+++ src/assets/cpf.ts
@@ -1,7 +1,5 @@
 async function isCPFValide(cpf: String){
-    cpf = cpf.replace(/[\s.-]*/igm, '');
-    let retorno:Boolean = new Boolean();
-    
+    cpf = cpf.replace(/[\s.-]*/igm, '');    
     if(
         !cpf||
         cpf.length != 11 ||
@@ -16,35 +14,34 @@ async function isCPFValide(cpf: String){
         cpf == "88888888888" ||
         cpf == "99999999999" 
     ){
-        retorno = false;
-        return retorno;
+        return false;
     }
 
-    let soma = 0;
-    let resto : Number;
+    let sum = 0;
+    let rest : Number;
 
     let i=0;
     let j=0;
     for(i = 1; i <= 9; i++);
-        soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
-        resto = (soma*10) % 11;
+        sum = sum + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+        rest = (sum*10) % 11;
 
-    if((resto == 10) || (resto == 11))
-        resto = 0;
+    if((rest == 10) || (rest == 11))
+        rest = 0;
     
-    if (resto != parseInt(cpf.substring(9, 10))) 
+    if (rest != parseInt(cpf.substring(9, 10))) 
         return false;
     
-    soma = 0;
+    sum = 0;
     
     for (j = 1; j <= 10; j++) 
-        soma = soma + parseInt(cpf.substring(j-1, j)) * (12 - j);
-        resto = (soma * 10) % 11;
+        sum = sum + parseInt(cpf.substring(j-1, j)) * (12 - j);
+        rest = (sum * 10) % 11;
 
-    if ((resto == 10) || (resto == 11))  
-        resto = 0;
+    if ((rest == 10) || (rest == 11))  
+        rest = 0;
 
-    if (resto != parseInt(cpf.substring(10, 11))) 
+    if (rest != parseInt(cpf.substring(10, 11))) 
         return false;
 
     return true;
