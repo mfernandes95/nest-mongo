e8ffa0c634a67daacde07d4aaec07bf603eaac87
Livia Dantas <livia.ss.dantas@gmail.com>
2020-12-03 11:55:35 -0300

    CPF validation

--- src/assets/cpf.ts
+++ src/assets/cpf.ts
@@ -1,50 +1,50 @@
-// async function isCPFValide(cpf: String){
-//     cpf = cpf.replace(/[\s.-]*/igm, '');    
-//     if(
-//         !cpf||
-//         cpf.length != 11 ||
-//         cpf == "00000000000" ||
-//         cpf == "11111111111" ||
-//         cpf == "22222222222" ||
-//         cpf == "33333333333" ||
-//         cpf == "44444444444" ||
-//         cpf == "55555555555" ||
-//         cpf == "66666666666" ||
-//         cpf == "77777777777" ||
-//         cpf == "88888888888" ||
-//         cpf == "99999999999" 
-//     ){
-//         return false;
-//     }
+async function isCPFValide(cpf: String){
+    cpf = cpf.replace(/[\s.-]*/igm, '');    
+    if(
+        !cpf||
+        cpf.length != 11 ||
+        cpf == "00000000000" ||
+        cpf == "11111111111" ||
+        cpf == "22222222222" ||
+        cpf == "33333333333" ||
+        cpf == "44444444444" ||
+        cpf == "55555555555" ||
+        cpf == "66666666666" ||
+        cpf == "77777777777" ||
+        cpf == "88888888888" ||
+        cpf == "99999999999" 
+    ){
+        return false;
+    }
 
-//     let sum = 0;
-//     let rest : Number;
+    let sum = 0;
+    let rest : Number;
 
-//     let i=0;
-//     let j=0;
-//     for(i = 1; i <= 9; i++);
-//         sum = sum + parseInt(cpf.substring(i - 1, i)) * (11 - i);
-//         rest = (sum*10) % 11;
+    let i=0;
+    let j=0;
+    for(i = 1; i <= 9; i++);
+        sum = sum + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+        rest = (sum*10) % 11;
 
-//     if((rest == 10) || (rest == 11))
-//         rest = 0;
+    if((rest == 10) || (rest == 11))
+        rest = 0;
     
-//     if (rest != parseInt(cpf.substring(9, 10))) 
-//         return false;
+    if (rest != parseInt(cpf.substring(9, 10))) 
+        return false;
     
-//     sum = 0;
+    sum = 0;
     
-//     for (j = 1; j <= 10; j++) 
-//         sum = sum + parseInt(cpf.substring(j-1, j)) * (12 - j);
-//         rest = (sum * 10) % 11;
+    for (j = 1; j <= 10; j++) 
+        sum = sum + parseInt(cpf.substring(j-1, j)) * (12 - j);
+        rest = (sum * 10) % 11;
 
-//     if ((rest == 10) || (rest == 11))  
-//         rest = 0;
+    if ((rest == 10) || (rest == 11))  
+        rest = 0;
 
-//     if (rest != parseInt(cpf.substring(10, 11))) 
-//         return false;
+    if (rest != parseInt(cpf.substring(10, 11))) 
+        return false;
 
-//     return true;
+    return true;
 
     
-// }
+}
