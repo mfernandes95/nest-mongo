82eb108a97e4e7e0d743c44cc9e25fa5a2f2a35d
Livia Dantas <livia.ss.dantas@gmail.com>
2020-12-03 15:56:05 -0300

    creating auth strategy

--- src/auth/shared/auth.service.ts
+++ src/auth/shared/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import{UserService} from '../../user/user.service'
+import { UserService } from '../../user/user.service';
 
 @Injectable()
 export class AuthService {
-    
-    constructor(
-        private userService: UserService,
-    ){}
+  constructor(private userService: UserService) {}
+  async validateUser(userEmail: String, userPassword: String) {
+    const user = await this.userService.findByEmail(userEmail);
+    if (user && user.password === userPassword) {
+      const { _id, name, email } = user;
+      return { id: _id, name, email };
+    }
+    return null;
+  }
 }
---
+++ src/auth/shared/local.strategy.ts
@@ -0,0 +1,15 @@
+import {Injectable, UnauthorizedException} from '@nestjs/common';
+import {Strategy} from 'passport-local';
+import {PassportStrategy} from '@nestjs/passport';
+import {AuthService} from './auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy){
+    constructor(private authService: AuthService){
+        super({
+            usernameField: 'email',
+            passwordField: 'password'
+        })
+
+    }
+}
