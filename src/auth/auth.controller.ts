d74fb71324f6c1a77d5a4b07446046ababba016c
Livia Dantas <livia.ss.dantas@gmail.com>
2020-12-06 17:02:33 -0300

    fix login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Request, Post } from '@nestjs/common';
+import { LocalAuthGuard } from './shared/local-auth.guard';
+
 
 @Controller()
-export class AuthController {}
+export class AuthController {
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req: any){
+    console.log('Login_Teste', req);
+    return req.user;
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,19 @@
-import { Controller, UseGuards, Request, Post } from '@nestjs/common';
-import { LocalAuthGuard } from './shared/local-auth.guard';
+import { Module } from "@nestjs/common";
+import { UserModule } from "src/user/user.module";
+import { AuthService } from "./shared/auth.service";
+import { LocalStrategy } from "./shared/local.strategy";
+import {AuthController} from "./auth.controller"
 
-
-@Controller()
-export class AuthModule {
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req: any){
-    return req.user;
-  }
-}
+@Module({
+  imports:[
+    UserModule,
+  ],
+  controllers: [
+    AuthController,
+  ],
+  providers:[
+    AuthService,
+    LocalStrategy
+  ]
+})
+export class AuthModule{}
