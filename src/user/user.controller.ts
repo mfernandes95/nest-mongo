920c3caf1d55226df386bf620aa87988415e2852
Livia Dantas <livia.ss.dantas@gmail.com>
2020-12-09 18:19:03 -0300

    Controllers with authguard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { LocalAuthGuard } from 'src/auth/shared/local-auth.guard';
+import {JwtAuthGuard} from 'src/auth/shared/jwt-auth.guard'
 
 @Controller('users')
 export class UserController {
@@ -17,22 +19,26 @@ export class UserController {
     return this.userService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':cpf')
   findOne(@Param('cpf') cpf: Number) {
     return this.userService.findOne(+cpf);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('email/:email')
   findByEmail(@Param('email') email: String) {
     console.log('emailllllllll', email);
     return this.userService.findByEmail(email);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':cpf')
   update(@Param('cpf') cpf: String, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+cpf, updateUserDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':cpf')
   remove(@Param('cpf') cpf: String) {
     return this.userService.remove(+cpf);
